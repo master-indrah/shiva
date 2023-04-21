e5625794d14d8263f8c4212e95a3e24c7a673a18
master-indrah <128244556+master-indrah@users.noreply.github.com>
2023-04-21 14:59:11 +0800

    Create unlock.sh

---
+++ unlock.sh
@@ -0,0 +1,8 @@
+1. unlock the netflix
+
+bash <(curl -sSL https://raw.githubusercontent.com/fscarmen/warp_unlock/main/unlock.sh)
+
+
+2. check the unlock
+
+bash <(curl -L -s https://raw.githubusercontent.com/lmc999/RegionRestrictionCheck/main/check.sh)
