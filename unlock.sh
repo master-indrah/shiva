7cb9ffdf6305e74264b9ee87f8c7c1a94de4ec27
master-indrah <128244556+master-indrah@users.noreply.github.com>
2024-01-20 23:53:36 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,4 +1,5 @@
 1. unlock the netflix
+wget -N https://cdn.jsdelivr.net/gh/fscarmen/warp/menu.sh && bash menu.sh
 
 bash <(curl -sSL https://raw.githubusercontent.com/fscarmen/warp_unlock/main/unlock.sh)
 
